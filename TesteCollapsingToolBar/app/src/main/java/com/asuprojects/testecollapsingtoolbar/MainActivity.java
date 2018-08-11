1b5a8061a42137381ae4c0ea507cedc86da20011
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-11 19:18:44 -0300

    Exemplo CollapsingToolbar

---
+++ TesteCollapsingToolBar/app/src/main/java/com/asuprojects/testecollapsingtoolbar/MainActivity.java
@@ -0,0 +1,24 @@
+package com.asuprojects.testecollapsingtoolbar;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Exemplo Collapse");
+        }
+    }
+}
