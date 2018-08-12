60252d3b1aaf2b0d91a389a1fcebb22dacff8180
Adilson S. Utiama <adilson.utiama@gmail.com>
2018-08-11 22:14:01 -0300

    Teste Toolbar customizado

---
+++ TesteToolbarCustom/app/src/main/java/com/asuprojects/testetoolbarcustom/MainActivity.java
@@ -0,0 +1,31 @@
+package com.asuprojects.testetoolbarcustom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar_main);
+        toolbar.setTitle("Custom Toolbar");
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+}
